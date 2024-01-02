d2b06980d79754c487ac816371aed3e578d4f482
Sourabha Bharadwaj B M <36269370+bmsohwinc@users.noreply.github.com>
2024-01-02 11:33:40 +0530

    Update main.cpp to include ordered_set
    Policy based data structure: Ordered set https://www.geeksforgeeks.org/ordered-set-gnu-c-pbds/

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
-using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+using namespace std;
+// To enable using ordered_set. Check https://www.geeksforgeeks.org/ordered-set-gnu-c-pbds/
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 
 #define ll long long
 #define pb push_back
